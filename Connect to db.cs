18f03eb7c345d64cdb4487123e2080e4aa0d3633
JaroslavKom <jaroslavkomarovskiy@gmail.com>
2024-09-25 16:59:16 +0700

    Таблица для просмотра и поиск

---
+++ Connect to db.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace WpfApp1
+{
+    internal class Connect_to_db
+    {
+        public static MySqlConnection GetDBConnection(string host, string port, string database, string username, string password)
+        {
+            String connString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + password;
+            MySqlConnection conn = new MySqlConnection(connString);
+            return conn;
+        }
+    }
+    class ConDB
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            INIManager manager = new INIManager(Path.Combine(Environment.CurrentDirectory, "connect.ini"));
+            string host = manager.GetPrivateString("main", "ip");
+            string port = manager.GetPrivateString("main", "port");
+            string database = manager.GetPrivateString("main", "name_db");
+            string username = manager.GetPrivateString("main", "profile");
+            string password = manager.GetPrivateString("main", "pass");
+            return Connect_to_db.GetDBConnection(host, port, database, username, password);
+        }
+    }
+    public class INIManager
+    {
+        private const int SIZE = 1024;
+        private string path = null;
+        public INIManager(string aPath)
+        {
+            path = aPath;
+        }
+        public INIManager() : this("") { }
+        public string GetPrivateString(string aSection, string aKey)
+        {
+            StringBuilder buffer = new StringBuilder(SIZE);
+            GetPrivateString(aSection, aKey, null, buffer, SIZE, path);
+            return buffer.ToString();
+        }
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileString")]
+        private static extern int GetPrivateString(string section, string key, string def, StringBuilder buffer, int size, string path);
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace WpfApp1
 {
@@ -20,10 +23,97 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        MySqlConnection conn = ConDB.GetDBConnection();
         public MainWindow()
         {
             InitializeComponent();
-            int i = 0;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ProcessLog();
+            Listboxes();
+        }
+        private void ProcessLog()
+        {
+            process_log_dg.ItemsSource = null;
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("select id_process, process_name, category_name, owning_division_name from process_log, `process`, category, owning_division where process_n = id_process and process_c = id_category and process_od = id_owning_division;", conn);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            process_log_dg.ItemsSource = table.DefaultView;
+            conn.Close();
+        }
+        private void Listboxes()
+        {
+            conn.Open();
+            MySqlCommand process_name = new MySqlCommand("SELECT process_name FROM zadanie.process;", conn);
+            MySqlDataReader read_pn = process_name.ExecuteReader();
+            while (read_pn.Read())
+            {
+                process_name_cb.Items.Add(read_pn.GetValue(0).ToString());
+            }
+            conn.Close();
+
+            conn.Open();
+            MySqlCommand category_name = new MySqlCommand("SELECT category_name FROM zadanie.category;", conn);
+            MySqlDataReader read_cn = category_name.ExecuteReader();
+            while (read_cn.Read())
+            {
+                category_name_cb.Items.Add(read_cn.GetValue(0).ToString());
+            }
+            conn.Close();
+
+            conn.Open();
+            MySqlCommand owning_division = new MySqlCommand("SELECT owning_division_name FROM zadanie.owning_division;", conn);
+            MySqlDataReader read_od = owning_division.ExecuteReader();
+            while (read_od.Read())
+            {
+                owning_division_cb.Items.Add(read_od.GetValue(0).ToString());
+            }
+            conn.Close();
+        }
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessLog();
+            process_name_cb.Text = "";
+            category_name_cb.Text = "";
+            owning_division_cb.Text = "";
+        }
+
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+            string process = "", category = "", ow_div = "";
+            string select_command = "";
+
+            if (process_name_cb.Text.Trim() != "")
+            {
+                process = process_name_cb.Text;
+                select_command += String.Format(" and process_name = '{0}'", process);
+            }
+            if (category_name_cb.Text.Trim() != "")
+            {
+                category = category_name_cb.Text;
+                select_command += String.Format(" and category_name = '{0}'", category);
+            }
+            if (owning_division_cb.Text.Trim() != "")
+            {
+                ow_div = owning_division_cb.Text;
+                select_command += String.Format(" and owning_division_name = '{0}'", ow_div);
+            }
+
+            process_log_dg.ItemsSource = null;
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("select id_process, process_name, category_name, owning_division_name from process_log, `process`, category, owning_division where process_n = id_process and process_c = id_category and process_od = id_owning_division" + select_command + ";", conn);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            process_log_dg.ItemsSource = table.DefaultView;
+            conn.Close();
+        }
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
